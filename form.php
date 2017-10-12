3e28fb1b48c4eef864c358d0929075307ac34357
Zachary Burk <zachburk7@gmail.com>
2017-10-12 12:12:59 -0400

    Fix paramters

--- form.php
+++ form.php
@@ -5,16 +5,16 @@ define('DB_USERNAME', 'ecabinetAdmin');
 define('DB_PASSWORD', 'ecabinetAdmin');
 define('DB_DATABASE', 'ecabinet');
 
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if(!$link) {
     die('Could not connect: ' . mysql_error());
 }
 
-$db_selected = mysqli_select_db($link, DB_NAME);
+$db_selected = mysqli_select_db($link, DB_DATABASE);
 
 if(!$db_selected) {
-    die('Cannot use ' . DB_NAME . ': ' . mysql_error());
+    die('Cannot use ' . DB_DATABASE . ': ' . mysql_error());
 }
 
 $fvalue="";
